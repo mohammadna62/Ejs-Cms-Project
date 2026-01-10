69f6c79968f53540486e9d901610ec5dc8d11f51
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-01-10 11:19:04 +0330

    add edit course title feature

--- controllers/course.js
+++ controllers/course.js
@@ -4,7 +4,13 @@ exports.create = async (req, res) => {
   try {
     const { title } = req.body;
 
-    // if (title) {}
+    const course = await coursesModel.findOne({ title });
+
+    if (course) {
+      return res.redirect("/courses", {
+        error: "",
+      });
+    }
 
     await coursesModel.create({ title });
 
@@ -36,5 +42,14 @@ exports.remove = async (req, res) => {
 };
 
 exports.edit = async (req, res) => {
-  // Codes
+    try {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    await coursesModel.findOneAndUpdate({ _id: id },{title});
+
+    res.redirect("/courses");
+  } catch (err) {
+    return res.status(500).json({ message: "OoOps! UnKnown server error !!" });
+  }
 };
